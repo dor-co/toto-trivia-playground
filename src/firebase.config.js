d78adab7c1de35718ce21f74883bb31620540ce0
dor cohen <dorc@moonsite.co.il>
2021-04-07 09:55:07 +0300

    fixed renders

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 var firebaseConfig = {
     apiKey: "AIzaSyD1ZNGirlaxWU4jZwdBl_9l_ppCTawzpbw",
     authDomain: "tototrivia.firebaseapp.com",
@@ -7,3 +9,8 @@ var firebaseConfig = {
     appId: "1:382271449481:web:cad213ffddb26cd4dfd958",
     measurementId: "G-HB8TWY4MNW"
   };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  const db=firebase.firestore();
+
+export default db;
